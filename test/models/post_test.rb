cef9cab1c4b9a0f775a4f018bf6fc7cb40e2c50e
Talaa Zina <talazina@gmail.com>
2019-05-13 11:36:40 +0100

    tests for post model

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,4 +4,21 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+  	@post = Post.new(body: "content1", user_id: 1)
+  end
+
+  test 'Valid content' do
+  	assert @post.valid?
+  end	
+  test 'Invalid post without body' do
+  	@post.body = nil
+  	refute @post.valid?
+  	assert_not_nil @post.errors[:body], 'no validation error for body present'
+  end	
+  test 'Invalid post without user'	 do
+  	@post.user_id = nil
+  	refute @post.valid?
+  	assert_not_nil @post.errors[:user_id], 'no validation error for user present'
+  end	
 end
